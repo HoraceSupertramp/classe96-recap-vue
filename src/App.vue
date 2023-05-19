55ad2ab49d1cb2303d499013a838fc311b106d5d
Francesco Orazini <horace.supertramp@gmail.com>
2023-05-19 17:06:41 +0200

    Primo commit

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<script>
+import Card from './components/Card.vue';
+export default {
+    components: {
+        Card,
+    },
+    data() {
+        return {
+            menu: {
+                items: [
+                    {
+                        name: 'Margherita',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit rhoncus turpis ac tincidunt.',
+                        image: 'margherita.jpeg',
+                        price: '6,00€'
+                    },
+                    {
+                        name: 'Capricciosa',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit rhoncus turpis ac tincidunt.',
+                        image: 'capricciosa.webp',
+                        price: '9,00€'
+                    },
+                    {
+                        name: 'Diavola',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit rhoncus turpis ac tincidunt.',
+                        image: 'diavola.jpeg',
+                        price: '10,00€'
+                    },
+                    {
+                        name: 'Vegetariana',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit rhoncus turpis ac tincidunt.',
+                        image: 'vegetariana.jpeg',
+                        price: '8,00€'
+                    },
+                    {
+                        name: 'Ananas',
+                        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit rhoncus turpis ac tincidunt.',
+                        image: 'ananas.png',
+                        price: '11,00€'
+                    }
+                ]
+            }
+        }
+    },
+}
+</script>
+
+<template>
+    <header>
+        <h1>Menu</h1>
+        <button>Checkout</button>
+    </header>
+    <main>
+        <ul class="menu">
+            <li v-for="(pizza, index) in menu.items" :key="index">
+                <Card
+                    :pippo="pizza.name"
+                    :description="pizza.description"
+                    :price="pizza.price"
+                    :image="pizza.image"
+                />
+            </li>
+        </ul>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    main {
+        .menu {
+            display: flex;
+            flex-flow: row wrap;
+            gap: 1.5em;
+
+            li {
+                flex: 0 0 auto;
+                width: calc((100% - ( 1.5em * 2)) / 3);                
+            }
+        }
+    }
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+    props: {
+        id: String,
+        name: String,
+        description: String,
+        price: String,
+        image: String,
+    },
+    methods: {
+        getImagePath: function(img) {
+            return new URL(`../assets/${img}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="pizza">
+        <img :src="getImagePath(image)">
+        <div class="info">
+            <h2 class="name">{{ name }}</h2>
+            <div class="description">{{ description }}</div>
+            <div class="footer">
+                <h3 class="price">{{ price }}</h3>
+                <div class="add-to-cart">
+                    <button>Order</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .pizza {
+        display: flex;
+        background: #fff;
+        border-radius: 8px;
+        overflow: hidden;
+        box-shadow: 0 2px 2px rgba(0, 0, 0, .2);
+
+        img {
+            overflow: hidden;
+            flex: 1 1 30%;
+            object-fit: cover;
+        }
+
+        .info {
+            flex: 1 1 70%;
+            text-align: left;
+            padding: .8em 1.2em;
+            border: 1px solid #ededed;
+            border-width: 1px 1px 1px 0;
+            border-radius: 0 8px 8px 0;
+        }
+
+        .footer {
+            display: flex;
+            flex-flow: row nowrap;
+            justify-content: space-between;
+            align-items: center;
+        }
+    }
+</style>
